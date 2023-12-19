cc415d3ea629ececb820c730f21df1e308d91e15
Robin Ceustermans <robin.ceustermans@outlook.com>
2023-12-19 21:19:35 +0100

    Create Categories table and seed it

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -5,7 +5,7 @@ namespace BulkyWeb.Models
     public class Category
     {
         [Key]
-        public int Id { get; set; } //if name is Id, then EF will know this is PK
+        public int Id { get; set; } //if name is just Id, then EF will know this is PK, otherwise data annotation of [Key] is needed
         [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
