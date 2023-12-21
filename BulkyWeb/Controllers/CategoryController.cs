c1db1ac90e80880c1e50ba57ec36fe1ea9c28aee
Robin Ceustermans <robin.ceustermans@outlook.com>
2023-12-21 20:27:44 +0100

    Create Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,5 +23,13 @@ namespace BulkyWeb.Controllers
             //you could pas a new class, but not needed as long as the class is defined in the view, could be useful to define a few properties beforehand
             //return View(new Category());
         }
+
+        [HttpPost] //aanotation has to be present
+        public IActionResult Create(Category obj)
+        { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; } //if name is just Id, then EF will know this is PK, otherwise data annotation of [Key] is needed
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Category Order")]
         public int DisplayOrder { get; set; }
     }
 }
